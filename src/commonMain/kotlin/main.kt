ce5f7844d3c8114d4aab3006706af217d913708a
TheElizarov <elizarov1988@gmail.com>
2024-04-28 12:28:23 +0400

    playground/apply-atlas-sprite-and-moving-it: Some refactoring - localize common code in one method; add comments; add base Config with constants

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -9,33 +9,62 @@ import korlibs.time.*
 import kotlin.math.*
 import kotlin.random.*
 
+object Config {
+    const val title = "Code Every Day - KorGE - Sunny Land"
+
+    const val width = 800
+    const val height = 600
+
+    val windowSize = Size(width, height)
+    val virtualSize = Size(width, height)
+
+    const val backgroundColorValue = "#2b2b2b"
+    val backgroundColors = Colors[backgroundColorValue]
+
+    const val randomMovingMin = 1.5
+    const val randomMovingMax = 5.0
+}
+
 suspend fun main() = Korge(
-    title = "Code Every Day - KorGE - Sunny Land",
-    windowSize = Size(800, 600),
-    virtualSize = Size(800, 600),
-    backgroundColor = Colors["#2b2b2b"]
+    title = Config.title,
+    windowSize = Config.windowSize,
+    virtualSize = Config.virtualSize,
+    backgroundColor = Config.backgroundColors
 ) {
     val spriteAtlas = resourcesVfs["Sunny-Land/atlas/atlas.json"].readAtlas()
+    testDisplayEagles(spriteAtlas)
+}
 
-    List(3) {
+/**
+ * Generate [count] Eagle Sprite from [Atlas]
+ * @return  list of Sprite with can random moving on the Window
+ */
+private fun Container.testDisplayEagles(
+    spriteAtlas: Atlas,
+    count: Int = 3
+) : List<Sprite> {
+    return List(count) {
         displaySprite(
             atlas = spriteAtlas,
             name = SpriteName.eagle,
-            position = Position(
-                x = Random.nextDouble(0.0, 512.0).toFloat(),
-                y = Random.nextDouble(0.0, 512.0).toFloat()
-            )
+            position = getRandomPosition()
         )
     }
 }
 
+/**
+ * Create Sprite from [Atlas] with prefix [name] and
+ * display it on Container with [Position].
+ * Sprite can scale with [scaleXY] and delay for frame display with [displayTime]
+ */
 private fun Container.displaySprite(
     atlas: Atlas,
     name: String,
     position: Position = Position(),
     scaleXY: Int? = 5,
-    displayTime: Double? = 100.0
-) {
+    displayTime: Double? = 100.0,
+    useRandomMoving: Boolean = true
+) : Sprite {
     val spriteAnimation = atlas.getSpriteAnimation(name)
 
     val sprite = sprite(spriteAnimation) {
@@ -50,24 +79,72 @@ private fun Container.displaySprite(
         spriteDisplayTime = TimeSpan(displayTime ?: 0.0)
     )
 
-    movingRandom(sprite)
+    if (useRandomMoving) {
+        movingRandom(sprite)
+    }
+
+    return sprite
 }
 
+/**
+ * Linear random moving [Sprite] on [Container] from 0 to 512
+ */
 private fun Container.movingRandom(
     sprite: Sprite
 ) {
-    var dx = Random.nextDouble(1.5, 4.5)
-    var dy = Random.nextDouble(1.5, 4.5)
+    var dx = getRandomMoving()
+    var dy = getRandomMoving()
     addUpdater {
-        if (sprite.x !in 0f..512f) {
-            dx = -1 * dx.sign * Random.nextDouble(1.5, 7.5)
+        if (sprite.x.isNotInContainerByWidth()) {
+            dx = dx.signInverse() * getRandomMoving()
         }
-        if (sprite.y !in 0f..512f) {
-            dy = -1 * dy.sign * Random.nextDouble(1.5, 7.5)
+        if (sprite.y.isNotInContainerByHeight()) {
+            dy = dy.signInverse() * getRandomMoving()
         }
-        sprite.x += dx.toFloat()
-        sprite.y += dy.toFloat()
+        sprite.x += dx
+        sprite.y += dy
     }
+}
+
+private fun Float.isNotInContainerByWidth(): Boolean {
+    return !isInContainerByWidth()
+}
+
+private fun Float.isInContainerByWidth(): Boolean {
+    return this in 0f..Config.windowSize.width
+}
+
+private fun Float.isNotInContainerByHeight(): Boolean {
+    return !isInContainerByHeight()
+}
+
+private fun Float.isInContainerByHeight(): Boolean {
+    return this in 0f..Config.windowSize.height
+}
+
+private fun getRandomPosition(): Position {
+    return Position(
+        x = getRandom(max = Config.windowSize.width.toDouble()),
+        y = getRandom(max = Config.windowSize.height.toDouble())
+    )
+}
+
+private fun getRandomMoving(
+    min: Double = Config.randomMovingMin,
+    max: Double = Config.randomMovingMax
+): Float {
+    return Random.nextDouble(min, max).toFloat()
+}
+
+private fun Float.signInverse(): Float {
+    return -1 * sign
+}
+
+private fun getRandom(
+    min: Double = 0.0,
+    max: Double
+): Float {
+    return Random.nextDouble(min, max).toFloat()
 }
 
 data class Position(
