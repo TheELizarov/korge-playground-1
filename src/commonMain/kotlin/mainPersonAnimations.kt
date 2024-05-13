1f24520918cf269ec8d552e50dd90f70926acb6c
TheElizarov <elizarov1988@gmail.com>
2024-05-13 21:00:06 +0400

    playground/person-animations: add logic for changing animations

--- src/commonMain/kotlin/mainPersonAnimations.kt
+++ src/commonMain/kotlin/mainPersonAnimations.kt
@@ -1,7 +1,13 @@
+import korlibs.event.*
 import korlibs.image.atlas.*
 import korlibs.io.file.std.*
 import korlibs.korge.*
+import korlibs.korge.input.*
+import korlibs.korge.view.*
+import korlibs.math.geom.*
+import korlibs.time.*
 import model.*
+import kotlin.time.Duration.Companion.milliseconds
 
 suspend fun initPersonAnimations() = Korge(
     title = Config.title,
@@ -11,4 +17,36 @@ suspend fun initPersonAnimations() = Korge(
 ) {
     val spriteAtlas = resourcesVfs["Sunny-Land/atlas/atlas.json"].readAtlas()
 
+    val playerClimb = spriteAtlas.getSpriteAnimation("player/climb")
+    val playerCrunch = spriteAtlas.getSpriteAnimation("player/crouch")
+    val playerHurt = spriteAtlas.getSpriteAnimation("player/hurt")
+
+    val playerIdle = spriteAtlas.getSpriteAnimation("player/idle")
+    val playerJump = spriteAtlas.getSpriteAnimation("player/jump")
+    val playerRun = spriteAtlas.getSpriteAnimation("player/run")
+
+    val spritePlayer = sprite(playerIdle)
+    spritePlayer.position(Point(200f, 200f))
+    spritePlayer.scaleXY = 10f
+    spritePlayer.playAnimationLooped(
+        spriteDisplayTime = TimeSpan(200.0)
+    )
+
+    addUpdater {
+        val animation = when {
+            views.input.keys[Key.RIGHT] -> {
+                spritePlayer.x += 1
+                playerRun
+            }
+
+            views.input.keys[Key.LEFT] -> {
+                spritePlayer.x -= 1
+                playerRun
+            }
+
+            views.input.keys[Key.UP] -> playerJump
+            else -> playerIdle
+        }
+        spritePlayer.playAnimation(animation)
+    }
 }
