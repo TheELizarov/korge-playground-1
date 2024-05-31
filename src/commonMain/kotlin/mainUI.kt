126a172f0e824c4241c1b4165435d02ff9ed9f69
TheElizarov <elizarov1988@gmail.com>
2024-05-31 11:00:31 +0400

    playground/ui: learn base property for button: change color, corners and others

--- src/commonMain/kotlin/mainUI.kt
+++ src/commonMain/kotlin/mainUI.kt
@@ -1,8 +1,12 @@
+import korlibs.image.color.*
 import korlibs.korge.*
 import korlibs.korge.ui.*
 import korlibs.korge.view.*
+import korlibs.math.geom.*
 import korlibs.render.*
 import model.*
+import ui.*
+import kotlin.random.*
 
 suspend fun initUI() = Korge(
     title = Config.title,
@@ -11,7 +15,12 @@ suspend fun initUI() = Korge(
     backgroundColor = Config.backgroundColors,
     quality = GameWindow.Quality.PERFORMANCE
 ) {
-    displayButtonClickCounter()
+    /**
+     * Example for button click counter
+     *  displayButtonClickCounter()
+     */
+
+    displayButton()
 }
 
 private var clickCounterByButton = 0
@@ -28,6 +37,59 @@ private fun Container.displayButtonClickCounter() {
         clicked {
             ++clickCounterByButton
             text = clickCounterButtonLabel
+        }
+    }
+}
+
+/**
+ * Example for learn base properties of [UIButton]
+ */
+private var isClicked = false
+private fun Container.displayButton() {
+    val button = uiButton(
+        label = "Start Code Every Day"
+    ) {
+        width = 200f
+        height = 100f
+        bgColorOver = Colors.DARKCYAN
+        textSize = 20f
+
+        clicked {
+            isClicked = isClicked.not()
+        }
+    }
+
+    animateChangingButtonRadius(button)
+    animateChangingButtonTextColor(button)
+}
+
+private fun Container.animateChangingButtonRadius(
+    button: UIButton
+) {
+    val maxRadius = 45f
+    val minRadius = 0f
+    var stepChangingRadius = 0.5
+    onEverySeconds(25.0) {
+        if (isClicked) {
+            val currentRadius = button.background.radius
+            if (currentRadius.topLeft > maxRadius || currentRadius.topLeft < minRadius) {
+                stepChangingRadius *= -1
+            }
+            button.background.radius = RectCorners(
+                currentRadius.topLeft + stepChangingRadius
+            )
+        }
+    }
+}
+
+private fun Container.animateChangingButtonTextColor(
+    button: UIButton
+) {
+    val colors = Colors.colorsByName.values.toList()
+    onEverySeconds(500.0) {
+        if (isClicked) {
+            val index = Random.nextInt(0, colors.size)
+            button.textColor = colors[index]
         }
     }
 }
