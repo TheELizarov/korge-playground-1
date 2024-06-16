80d0d14f74320d1388dfc611e53bbb811ea52b2b
TheElizarov <elizarov1988@gmail.com>
2024-06-16 21:14:37 +0400

    playground/ui: add high level methods for prototype

--- src/commonMain/kotlin/mainUiUseInputs.kt
+++ src/commonMain/kotlin/mainUiUseInputs.kt
@@ -1,6 +1,10 @@
 import korlibs.korge.*
+import korlibs.korge.ui.*
+import korlibs.korge.view.*
 import korlibs.render.*
 import model.*
+
+private val content = mutableListOf<String>()
 
 suspend fun initUiUseInputs() = Korge(
     title = Config.title,
@@ -9,5 +13,18 @@ suspend fun initUiUseInputs() = Korge(
     backgroundColor = Config.backgroundColors,
     quality = GameWindow.Quality.PERFORMANCE
 ) {
+    displayInputs()
+    displayContent()
+}
 
+private fun Container.displayInputs() {
+
+}
+
+private fun Container.displayContent() {
+
+}
+
+private fun Container.getContentItemView(): View? {
+   return null
 }
