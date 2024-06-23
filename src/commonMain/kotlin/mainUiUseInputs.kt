e886bfde8e29ce22e27d5664c1836d9f56e91877
TheElizarov <elizarov1988@gmail.com>
2024-06-23 20:40:58 +0400

    playground/ui: add high level methods for prototype - add comments

--- src/commonMain/kotlin/mainUiUseInputs.kt
+++ src/commonMain/kotlin/mainUiUseInputs.kt
@@ -6,6 +6,12 @@ import model.*
 
 private val content = mutableListOf<String>()
 
+/**
+ * Simple example for using inputs for creating
+ * list of items
+ *
+ * Items can be deleting
+ */
 suspend fun initUiUseInputs() = Korge(
     title = Config.title,
     windowSize = Config.windowSize,
@@ -17,10 +23,16 @@ suspend fun initUiUseInputs() = Korge(
     displayContent()
 }
 
+/**
+ * Display input for create new item
+ */
 private fun Container.displayInputs() {
 
 }
 
+/**
+ * Display list of created items from [displayInputs]
+ */
 private fun Container.displayContent() {
 
 }
